7788eea994b4adca7c47bfacfda04dbd205f26c4
aleksandresukhitashvili@gmail.com <aleksandresukhitashvili@gmail.com>
2017-08-20 12:04:02 +0400

    Added RunningMethodLogger

---
+++ TextLoggerNet/LoggingTools/IRunningMethodLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace TextLoggerNet.LoggingTools
+{
+    public interface IRunningMethodLogger
+    {
+        void Dispose();
+        void ResetAction(string newName);
+        void SetActionName(string message);
+        void Start();
+    }
+}
---
+++ TextLoggerNet/LoggingTools/RunningMethodLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextLoggerNet.Interfaces;
+
+namespace TextLoggerNet.LoggingTools
+{
+    public class RunningMethodLogger : IDisposable, IRunningMethodLogger
+    {
+        private readonly ITextLogger _textLogger;
+        private readonly System.Timers.Timer _timer;
+        string _currentActivityName = "start";
+        int _internalCounter;
+
+        public RunningMethodLogger(ITextLogger textLogger, TimeSpan updateInterval)
+        {
+            _textLogger = textLogger;
+            _timer = new System.Timers.Timer(updateInterval.TotalMilliseconds);
+            _timer.Elapsed +=
+                (s, e) => { textLogger.WriteLine($"\t\t\t{TimeSpan.FromSeconds(++_internalCounter * updateInterval.TotalSeconds).ToVerboseStingHMS()} Since {_currentActivityName}"); };
+        }
+
+        public void ResetAction(string newName)
+        {
+            _currentActivityName = newName;
+            _textLogger.WriteLine("===> " + _currentActivityName);
+            _internalCounter = 0;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void SetActionName(string message) => _currentActivityName = message;
+
+        public void Start() => _timer.Start();
+    }
+
+}
